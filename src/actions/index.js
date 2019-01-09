0d43ee77a35083d76f657c99657b626309dcab1e
Jaime Ortiz <jxme88@gmail.com>
2019-01-09 13:36:10 -0800

    CRUD actions creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,13 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import streams from '../apis/streams';
+import { 
+    SIGN_IN, 
+    SIGN_OUT, 
+    CREATE_STREAM, 
+    FETCH_STREAM, 
+    FETCH_STREAMS, 
+    DELETE_STREAM, 
+    EDIT_STREAM } 
+    from './types';
 
 export const signIn = userId => { //receives google user id from authReducer.
     return {
@@ -12,3 +21,43 @@ export const signOut = () => {
         type: SIGN_OUT//string that being imported from the type.js file
     };
 };
+
+//REST CRUD 
+
+//POST | Create - record | Route - /streams | single record
+
+export const createStream = formValues => async dispatch => { //
+    const response = await streams.post('./streams', formValues);
+    
+    dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+//GET | GET ALL - List | Route - /streams | list of records
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+//Get | GET BY ID - single record | Route - /streams/:id | single record
+export const fetchStream = id => async dispatch => { 
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+//UPDATE | PUT - single record | Route - /streams/:id | edit single record
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+//DELETE | DELETE - single record | Route - /streams/:id | delete single record
+export const deleteStream = id => async dispatch => {
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({ type: DELETE_STREAM, payload: id });
+};
+
+
--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,9 @@ export const SIGN_OUT = "SIGN_OUT";
 
 //reason why were are putting the types into strings is because 
 //if we make an error we can quickly track the error. It will most likely be a speelling error.
+
+export const CREATE_STREAM = 'CREATE_STREAM'; //POST
+export const FETCH_STREAMS = 'FETCH_STREAMS'; //GET ALL
+export const FETCH_STREAM = 'FETCH_STREAM'; //GET BY ID
+export const DELETE_STREAM = 'DELETE_STREAM'; //DELETE
+export const EDIT_STREAM = 'EDIT_STREAM'; //UPDATE
